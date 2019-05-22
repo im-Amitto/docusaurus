a34cac7cd48957279c8521b1a075a2d75909ac59
im-Amitto <cse160001004@iiti.ac.in>
2019-05-22 14:35:33 +0530

    build removed

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -1,8 +1,8 @@
 const siteConfig = {
   title: 'BigBinary Ebook', // Title for website.
   tagline: 'markdown-book-generator ',
-  url: 'https://im-amitto.github.io/docusaurus/', // website URL
-  baseUrl: 'https://im-amitto.github.io/docusaurus/website/build/bigbinary-ebook/',
+  url: 'http://localhost/', // website URL
+  baseUrl: 'http://localhost/',
 
   projectName: 'bigbinary-ebook',
   organizationName: 'bigbinary',
